b29562f67e2e4f24361f5ec7c182623e89033db3
iti12 <41358336+iti12@users.noreply.github.com>
2025-12-10 18:24:14 +0200

    Delete test_metrics_handler.py

--- test_metrics_handler.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-
-@pytest.mark.asyncio
-async def test_metrics(client):
-    r = await client.get("/metrics")
-    assert r.status == 200
-    data = await r.json()
-    assert "switches" in data
